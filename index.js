96d381dc02e32bc9232ec8f92ee2115653387880
Medrine-Jepkemoi <jepkemoimedrine@gmail.com>
2023-03-22 20:41:28 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer = 'not bob';
+const leastFavoriteCustomer = 'Jane';
+
+function upperCaseCustomerName(){
+
+    console.log(customerName.toUpperCase());
+
+}
+
+console.log(upperCaseCustomerName())
+
+function setBestCustomer(){
+
+    console.log(bestCustomer);
+}
+
+console.log(setBestCustomer())
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+
+    return bestCustomer;
+}
+
+console.log(overwriteBestCustomer())
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Mary';
+
+    return leastFavoriteCustomer;
+}
+
+// console.log(changeLeastFavoriteCustomer())
